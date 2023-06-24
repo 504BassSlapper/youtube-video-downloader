c0fe5ac93cfe980f8a4ba147a610d6132e5df7e3
504BassSlapper <504bassslapper@gmail.com>
2023-06-24 23:53:07 +0200

    update project modulation and managing downloads

--- getVideosFromLibrary.py
+++ getVideosFromLibrary.py
@@ -1,30 +1,7 @@
 from pytube import YouTube
 from pytube import Playlist
-import os
-import shutil
+from moove_file import move_files_to_subdirectory
 
-
-def move_files_to_subdirectory(src_dir, dest_dir):
-    # Create the destination directory if it doesn't exist
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-
-    files = os.listdir(src_dir)
-    for file in files:
-        if file.endswith(".mp4"):
-            new_file_name = os.path.splitext(file)[0] + ".mp3"
-
-            # get source and destination file path before mv
-            src_file_path = os.path.join(src_dir, file)
-            dest_file_path = os.path.join(dest_dir, new_file_name)
-            # performing mv
-            try:
-                shutil.move(src_file_path, dest_file_path)
-                print(
-                    f"The file '{src_file_path}' have been renamed and mooved successfully"
-                )
-            except:
-                print(f"could not transfert '{src_file_path}' to '{dest_file_path}'")
 
 
 def DownloadSingleVideo(link):
--- moove_file.py
+++ moove_file.py
@@ -10,8 +10,10 @@ def move_files_to_subdirectory(src_dir, dest_dir):
     files = os.listdir(src_dir)
     for file in files:
         if file.endswith(".mp4"):
-            new_file_name = os.path.splitext(file)[0] + ".mp3"
-
+            if dest_dir == "mp3":
+                new_file_name = os.path.splitext(file)[0] + ".mp3"
+            else:
+                new_file_name = file
             # get source and destination file path before mv
             src_file_path = os.path.join(src_dir, file)
             dest_file_path = os.path.join(dest_dir, new_file_name)
@@ -24,5 +26,3 @@ def move_files_to_subdirectory(src_dir, dest_dir):
             except:
                 print(f"could not transfert '{src_file_path}' to '{dest_file_path}'")
 
-
-move_files_to_subdirectory(".", "mp3")
